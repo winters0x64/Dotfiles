d152b25e98a8b4cfbb80f2a6cd6d920a8a01bb0d
winters <winters0x64>
2023-03-07 19:32:15 +0530

    Modularised version

---
+++ Celestia_Monochrome/polybar/scripts/reboot.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# Change permission of reboot in the sudoers file
+
+# winters  ALL= NOPASSWD: /usr/bin/shutdown, /usr/bin/reboot
+
+sudo reboot
---
+++ Celestia_Monochrome/polybar/scripts/turnoff.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+# You need to change the permission in the sudoers file to not prompt for root pass word for this script
+
+sudo shutdown now
